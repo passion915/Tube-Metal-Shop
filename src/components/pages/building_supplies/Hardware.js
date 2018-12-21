b625e0c2caf106c2fdf48631e83461d93fc8ad9c
hipolitical1 <Info@ozbeatz.com>
2018-12-22 06:30:42 +0800

    Hardware skeleton

---
+++ src/components/pages/building_supplies/Hardware.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class Hardware extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { isCity: false };
+    }
+    
+    
+
+    componentDidMount() {
+        
+    }
+
+    
+    render() {
+        return (
+            <div>
+                <h2>Hardware Page</h2>
+            </div>
+        )
+    }
+}
+ 
+
+export default Hardware;
