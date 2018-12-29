dcb7d03c0c712ebdf5d03f67be405b6da8d19c8f
hipoliticaltutorials <hipoliticaltutorials@gmail.com>
2018-12-30 04:48:46 +0800

    update

--- src/components/pages/building_supplies/Hardware.js
+++ src/components/pages/building_supplies/Hardware.js
@@ -17,7 +17,7 @@ class Hardware extends React.Component {
     render() {
         return (
             <div>
-                <h2>Hardware Page test 1</h2>
+                <h2>Hardware Page test 11</h2>
             </div>
         )
     }
