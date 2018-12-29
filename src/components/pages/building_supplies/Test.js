4adb23754289c13ae5ab37d32b75c2bd19dbdadb
hipoliticaltutorials <hipoliticaltutorials@gmail.com>
2018-12-30 04:51:47 +0800

    update

---
+++ src/components/pages/building_supplies/Test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class Test extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { isCity: false };
+    }
+    
+    
+
+    componentDidMount() {
+        
+    }
+
+    
+    render() {
+        return (
+            <div>
+                <h2>Hardware Page test 11</h2>
+            </div>
+        )
+    }
+}
+ 
+
+export default Test;
