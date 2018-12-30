1fd5c647422b7e64dcc0f7629f0b2824a5f501cb
hipolitical1 <Info@ozbeatz.com>
2018-12-30 16:18:51 +0800

    update

--- src/components/pages/building_supplies/Test.js
+++ src/components/pages/building_supplies/Test.js
@@ -17,7 +17,7 @@ class Test extends React.Component {
     render() {
         return (
             <div>
-                <h2>Hardware Page 11</h2>
+                <h2>Hardware Page 222</h2>
             </div>
         )
     }
