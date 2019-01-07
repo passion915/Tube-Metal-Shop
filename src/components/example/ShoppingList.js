25f4db7ea19a4845e0483ac77b391dca89506b94
hipolitical1 <Info@ozbeatz.com>
2019-01-07 23:11:24 +0800

    update

---
+++ src/components/example/ShoppingList.js
@@ -0,0 +1,15 @@
+
+class ShoppingList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>    
+      </div>
+    )
+  }  
+}
