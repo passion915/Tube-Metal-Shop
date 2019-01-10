eee951dc3fd0e1bc8acf6c062390f4be9be6fb93
hipolitical1 <Info@ozbeatz.com>
2019-01-11 01:49:10 +0800

    update

--- src/index.js
+++ src/index.js
@@ -52,7 +52,7 @@ class Game extends React.Component {
                     <Board />
                 </div>
                 <div className="game-info">
-                    <div>{/* status test */}</div>
+                    <div>{/* status */}</div>
                     <ol>{/* TODO */}</ol>
                 </div>
             </div>
