b29e7f4ed3b602aff881570b4ca9ab377175219b
ko915 <ko915@outlook.com>
2019-03-16 07:48:03 +0800

    update

--- src/components/pages/Test/Trusses.js
+++ src/components/pages/Test/Trusses.js
@@ -17,7 +17,7 @@ class Trusses extends React.Component {
     render() {
         return (
             <div>
-                <h2>Trusses Page test123</h2>
+                <h2>Trusses Page test1234</h2>
             </div>
         )
     }
