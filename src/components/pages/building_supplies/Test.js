f2f9c9d749e7d095dafce84285a7b7215e7f4468
ko915 <ko915@outlook.com>
2019-05-11 14:39:04 -0400

    update

--- src/components/pages/building_supplies/Test.js
+++ src/components/pages/building_supplies/Test.js
@@ -17,7 +17,7 @@ class Test extends React.Component {
     render() {
         return (
             <div>
-                <h2>Hardware Page 1234</h2>
+                <h2>Hardware Page 12345</h2>
             </div>
         )
     }
