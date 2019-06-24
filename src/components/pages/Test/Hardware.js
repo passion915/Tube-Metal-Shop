6d5fddd184d50c4fe92711cdc3b03c0063c9d72a
Leon-915 <ko915@outlook.com>
2019-06-24 04:08:33 -0400

    update

--- src/components/pages/Test/Hardware.js
+++ src/components/pages/Test/Hardware.js
@@ -17,7 +17,7 @@ class Hardware extends React.Component {
     render() {
         return (
             <div>
-                <h2>Hardware Page test 123</h2>
+                <h2>Hardware Page test 1234</h2>
             </div>
         )
     }
